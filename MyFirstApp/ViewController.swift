954cd84e7d26349a749204cc751cdf8bbab4ac7b
youngju <asdadssad@naver.coim>
2018-03-07 15:42:02 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by D7703_27 on 2018. 3. 7..
+//  Copyright © 2018년 D7703_27. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
